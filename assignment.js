e3020fcad75fb058bd5e4bb72adcbb862678defd
Moses Femi Smauel <mosesfemi10@gmail.com>
2025-01-13 15:13:04 +0100

    assignment correction

--- assignment.js
+++ assignment.js
@@ -2,17 +2,17 @@
 const express = require('express');
 const app = express();
 
-app.use('/', (req, res, next) => {
-    console.log('This is first middleware');
-    app.send('Welcome to my assignment page');
-    next();
-});
-
-app.use('/users', (req, res) => {
+app.use('/users', (req, res,) => {
     console.log('The second middleware');
     res.send('This get the client to users page');
 });
 
+app.use('/', (req, res, next) => {
+    console.log('This is first middleware');
+    res.send('Welcome to my assignment page');
+    next();
+});
+
 app.listen(3000, () => {
-    console.log(`Server running on localhost;3000`);
+    console.log(`Server running on localhost:3000`);
 });
